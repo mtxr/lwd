f6bd1fcc7cc455c73b106f19489ec082eeeabe24
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-05-19 18:30:13 -0300

    Fri May 19 18:30:13 -03 2017

--- bin/lwd.sh
+++ bin/lwd.sh
@@ -6,19 +6,25 @@ fi
 
 SCRIPT=`realpath -s $0`
 SCRIPTPATH=`dirname $SCRIPT`
+LWDJS=$SCRIPTPATH/lwd.js
+
+function cd() {
+    builtin cd $@
+    node $LWDJS add $@
+}
 
 function _complete_lwd() {
     local cur
     local IFS=$'\n'
     cur="${COMP_WORDS[COMP_CWORD]}"
-    local opts=$(node $SCRIPTPATH/lwd.js search "$cur")
+    local opts=$(node $LWDJS search "$cur")
     COMPREPLY=( ${opts[@]} )
     return 0
 }
 
 function lwd() {
     if [ "$#" = "0" ]; then
-        cd $(node $SCRIPTPATH/lwd.js)
+        cd $(node $LWDJS)
         return 0
     fi
     cd $@
