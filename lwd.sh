04648632fecdaa7bb50d369308508b79d90e5691
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-12-14 12:32:51 -0200

    Update lwd.sh

--- lwd.sh
+++ lwd.sh
@@ -17,20 +17,30 @@ lwd() {
   builtin cd "$last" && return 0
 }
 
-__lwd_preexec_fn_hook () {
-  if [ "$1" = "" ];then
-    return 0
+__LWDREGISTER=0
+
+__lwd_precmd_fn_hook () {
+  if [ $__LWDREGISTER -eq 0 ]; then
+    return
   fi
+  __LWDREGISTER=0
   (echo -n "$PWD" > $LWDHISTORY &)
 }
 
+__lwd_preexec_fn_hook () {
+  __LWDREGISTER=1
+}
+
 if [ -n "$ZSH_VERSION" ]; then
+  if [[ ! " ${precmd_functions[@]} " =~ " __lwd_precmd_fn_hook " ]]; then
+    precmd_functions=(${precmd_functions[@]} "__lwd_precmd_fn_hook")
+  fi
   if [[ ! " ${preexec_functions[@]} " =~ " __lwd_preexec_fn_hook " ]]; then
     preexec_functions=(${preexec_functions[@]} "__lwd_preexec_fn_hook")
   fi
 else
   # override current cd fn to allow adding to lwd history
   cd() {
-    builtin cd $@ && __lwd_preexec_fn_hook && return 0
+    builtin cd $@ && __lwd_precmd_fn_hook && return 0
   }
 fi
